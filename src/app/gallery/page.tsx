f29de7fb5b045384ef5b041c32bfbff2bd8fd2a9
Zywooo <blackyyy1206@gmail.com>
2026-04-21 14:46:25 +0545

    gallery lai photos

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,30 +1,30 @@
 import { Flex, Meta, Schema } from "@once-ui-system/core";
-import GalleryView from "@/components/gallery/GalleryView";
-import { baseURL, gallery, person } from "@/resources";
+import GalleryView from "@/components/gallery/photosView";
+import { baseURL, photos, person } from "@/resources";
 
 export async function generateMetadata() {
   return Meta.generate({
     title: photos.title,
-    description: gallery.description,
+    description: photos.description,
     baseURL: baseURL,
-    image: `/api/og/generate?title=${encodeURIComponent(gallery.title)}`,
-    path: gallery.path,
+    image: `/api/og/generate?title=${encodeURIComponent(photos.title)}`,
+    path: photos.path,
   });
 }
 
-export default function Gallery() {
+export default function photos() {
   return (
     <Flex maxWidth="l">
       <Schema
         as="webPage"
         baseURL={baseURL}
-        title={gallery.title}
-        description={gallery.description}
-        path={gallery.path}
-        image={`/api/og/generate?title=${encodeURIComponent(gallery.title)}`}
+        title={photos.title}
+        description={photos.description}
+        path={photos.path}
+        image={`/api/og/generate?title=${encodeURIComponent(photos.title)}`}
         author={{
           name: person.name,
-          url: `${baseURL}${gallery.path}`,
+          url: `${baseURL}${photos.path}`,
           image: `${baseURL}${person.avatar}`,
         }}
       />
